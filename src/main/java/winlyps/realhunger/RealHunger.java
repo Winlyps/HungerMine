e4da3ddb85a145862d12f8a5ace6e0d08fcdf676
Winlypse <nts3@o2.pl>
2024-08-13 20:43:48 +0200

    Now the player loses hunger 2x slower ( because it was too rough ) for mining / building + breaking, harvesting the crops won't make the player lose food points

--- src/main/java/winlyps/realhunger/RealHunger.java
+++ src/main/java/winlyps/realhunger/RealHunger.java
@@ -1,5 +1,7 @@
 package winlyps.realhunger;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -24,22 +26,45 @@ public final class RealHunger extends JavaPlugin {
     private class BlockListener implements org.bukkit.event.Listener {
         @EventHandler(priority = EventPriority.MONITOR)
         public void onBlockBreakEvent(BlockBreakEvent event) {
-            blockCount++;
-            if (blockCount == 15) {
-                Player player = event.getPlayer();
-                player.setFoodLevel(Math.max(0, player.getFoodLevel() - 2));
-                blockCount = 0;
+            Block block = event.getBlock();
+            if (!isCrop(block.getType())) {
+                blockCount++;
+                if (blockCount == 30) {
+                    Player player = event.getPlayer();
+                    player.setFoodLevel(Math.max(0, player.getFoodLevel() - 2));
+                    blockCount = 0;
+                }
             }
         }
 
         @EventHandler(priority = EventPriority.MONITOR)
         public void onBlockPlaceEvent(BlockPlaceEvent event) {
             blockCount++;
-            if (blockCount == 15) {
+            if (blockCount == 30) {
                 Player player = event.getPlayer();
                 player.setFoodLevel(Math.max(0, player.getFoodLevel() - 2));
                 blockCount = 0;
             }
+        }
+
+        private boolean isCrop(Material material) {
+            return material == Material.WHEAT ||
+                    material == Material.CARROTS ||
+                    material == Material.POTATOES ||
+                    material == Material.BEETROOTS ||
+                    material == Material.NETHER_WART ||
+                    material == Material.COCOA ||
+                    material == Material.SUGAR_CANE ||
+                    material == Material.BAMBOO ||
+                    material == Material.CACTUS ||
+                    material == Material.SWEET_BERRY_BUSH ||
+                    material == Material.KELP_PLANT ||
+                    material == Material.KELP ||
+                    material == Material.GLOW_BERRIES ||
+                    material == Material.RED_MUSHROOM ||
+                    material == Material.BROWN_MUSHROOM ||
+                    material == Material.CRIMSON_FUNGUS ||
+                    material == Material.WARPED_FUNGUS;
         }
     }
 }
